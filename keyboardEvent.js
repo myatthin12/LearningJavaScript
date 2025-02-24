b4130ee92586f61c2cf338afa7b95505f18e569b
“myatthin12” <myatthinkhaing1234@gmail.com>
2025-02-24 11:50:38 +0630

    KeyboardEvent

--- keyboardEvent.js
+++ keyboardEvent.js
@@ -1,3 +1,14 @@
-document.addEventListener("keydown",function(event){
-    console.log("Pressed Key : ", event.key);
+const inputData = document.getElementById("inputField");
+const outputData = document.getElementById("output");
+
+inputData.addEventListener("keydown",function(event){
+    console.log("Keydown : ", event.key );
 });
+
+inputData.addEventListener("keyup", function(event){
+    outputData.innerText = inputData.value;
+    console.log("KeyUp : ",event.key);
+});
+
+
+
